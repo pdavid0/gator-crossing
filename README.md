a3e23c0c451545abc36047c6144a733e1bbf336c
Philippe David <philippe@inspectify.com>
2024-10-16 00:57:37 -0400

    add html version

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ https://projects.raspberrypi.org/en/projects/getting-started-with-git/3
 # Pico-8 Controller
 https://pico-8.fandom.com/wiki/Controllers
 https://github.com/mdqinc/SDL_GameControllerDB/blob/master/gamecontrollerdb.txt
+
+# exporting 
+https://www.lexaloffle.com/bbs/?pid=81260#p
--- bin/build.sh
+++ bin/build.sh
@@ -1,1 +1,6 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
+#/Applications/PICO-8.app/Contents/MacOS/pico8 game.p8 -export "-i 32 -s 2 -c 12 dist/gator.bin game.p8 high_score.p8"
+/Applications/PICO-8.app/Contents/MacOS/pico8 game.p8 -export "-i 32 -s 2 -c 12 -p one_button index.html game.p8"
+/Applications/PICO-8.app/Contents/MacOS/pico8 game.p8 -export "-i 32 -s 2 -c 12 -p one_button dist/gator.bin game.p8"
+/Applications/PICO-8.app/Contents/MacOS/pico8 game.p8 -export "-i 32 -s 2 -c 12 -p one_button dist/music%D.wav game.p8"
+/Applications/PICO-8.app/Contents/MacOS/pico8 game.p8 -export "-i 32 -s 2 -c 12 -p one_button dist/sprite.png game.p8"
